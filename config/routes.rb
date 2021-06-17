4c3ad7328875b2b1e718a62a7af95f8219ddca64
wangshuaijie <wangshuaijie@hotmail.com>
2021-06-17 15:57:06 +0000

    finish sign up page

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
+  
+  resources :users
 
   # root 'application#hello'
 end
