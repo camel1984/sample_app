0c38470099779134bde7fd1ed0fdf922bc72c75a
wangshuaijie <wangshuaijie@hotmail.com>
2021-07-09 15:06:33 +0000

    implement basic login

--- config/routes.rb
+++ config/routes.rb
@@ -14,4 +14,8 @@ Rails.application.routes.draw do
   resources :users
 
   # root 'application#hello'
+  
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
 end
